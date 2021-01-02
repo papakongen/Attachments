a83cc386da7795046cac9c5c0e1f0ec05fd0f977
papakongen <61790244+papakongen@users.noreply.github.com>
2021-01-02 02:13:20 +0100

    Update server.lua

--- papa-politi_attachments/server.lua
+++ papa-politi_attachments/server.lua
@@ -17,7 +17,7 @@ RegisterNetEvent("papapoliti:permission")
 AddEventHandler("papapoliti:permission", function()
 	local user_id = vRP.getUserId({source})
 
-	if vRP.hasGroup({user_id, "Politi"}) then
+	if vRP.hasGroup({user_id, "Politi-Job"}) then
         TriggerClientEvent("papapoliti:tag", source)
     else
         TriggerClientEvent('mythic_notify:client:DoCustomHudText', source, { type = 'error', text = 'Du ikke ikke adgang'}, 5000)
